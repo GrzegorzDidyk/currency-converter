e42b7e1aa238e20c360a9e6645b81fe445a97ee8
Grzegorz Didyk <grzegorz.didyk@gmail.com>
2022-08-16 21:40:34 +0200

    function displayCurrentRate added

--- js/script.js
+++ js/script.js
@@ -1,44 +1,51 @@
 {
-const formElement = document.querySelector(".js-form");
-const euroRate = 4.7241;
-const dolarRate = 4.2193;
-const funtRate = 5.6593;
-const currencyElement = document.querySelector(".js-currencySellect");
-const exchangeRateElement = document.querySelector(".js-exchangeRate");
-const amountPlnElement = document.querySelector(".js-amountPln");
-const resultElement = document.querySelector(".js-result");
+
+    const displayCurrentRate = (currency, exchangeRateElement) => {
+        const euroRate = 4.7241;
+        const dolarRate = 4.2193;
+        const funtRate = 5.6593;
+        switch (currency) {
+            case "EUR":
+                exchangeRateElement.value = euroRate;
+                break;
+            case "USD":
+                exchangeRateElement.value = dolarRate;
+                break;
+            case "GBP":
+                exchangeRateElement.value = funtRate;
+                break;
+            case "":
+                exchangeRateElement.value = "";
+                break;
+        }
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        const currencyElement = document.querySelector(".js-currencySellect");
+        const exchangeRateElement = document.querySelector(".js-exchangeRate");
+        const currency = currencyElement.value;
 
 
-formElement.addEventListener("input", () => {
-    const currency = currencyElement.value;
-    switch (currency) {
-        case "EUR":
-            exchangeRateElement.value = euroRate;
-            break;
-        case "USD":
-            exchangeRateElement.value = dolarRate;
-            break;
-        case "GBP":
-            exchangeRateElement.value = funtRate;
-            break;
-        case "":
-            exchangeRateElement.value = "";
-            break;
+        formElement.addEventListener("input", displayCurrentRate(currency, exchangeRateElement));
+
+        formElement.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+
+            const amountPlnElement = document.querySelector(".js-amountPln");
+            const resultElement = document.querySelector(".js-result");
+            const amountPln = amountPlnElement.value;
+            const exchangeRate = exchangeRateElement.value;
+            let result = amountPln / exchangeRate;
+
+            resultElement.innerHTML = `Za ${amountPln} PLN otrzymasz <strong> ${result.toFixed(2)} ${currency} </strong>`;
+        });
+
+        formElement.addEventListener("reset", (event) => {
+            resultElement.innerHTML = `Za x PLN otrzymasz`;
+        });
     }
-});
+    init();
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const currency = currencyElement.value;
-    const amountPln = amountPlnElement.value;
-    const exchangeRate = exchangeRateElement.value;
-    let result = amountPln/exchangeRate;
-
-    resultElement.innerHTML = `Za ${amountPln} PLN otrzymasz <strong> ${result.toFixed(2)} ${currency} </strong>`;
-});
-
-formElement.addEventListener("reset", (event) => {
-    resultElement.innerHTML = `Za x PLN otrzymasz`;
-});
 }
